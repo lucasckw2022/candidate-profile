11edf0b8b581142d8a803294e66729f9ab3eda4f
Lucas Cheung <lucas.k.cheung@accenture.com>
2018-08-13 18:21:35 +0800

    updated for heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require("path");
 const app = express();
 
-app.use(express.static(__dirname + 'dist'))
-.get('*', (req, res) => res.sendFile(path.join(__dirname + 'dist/index.html')))
+app.use(express.static(__dirname + '/dist'))
+.get('*', (req, res) => res.sendFile(path.join(__dirname + '/dist/index.html')))
 
 app.listen(process.env.PORT || 8080);
