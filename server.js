1765efe6231c6657b17f6c6d1b293cf0171f3974
Lucas Cheung <lucas.k.cheung@accenture.com>
2018-08-13 18:48:17 +0800

    test deploy to heroku

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require('express');
 const path = require("path");
 const app = express();
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
     next();
 })
 .use(express.static(__dirname + '/dist'))
