c103bc0f00700b00986014213bbfb9e0ff767f16
Lucas Cheung <lucas.k.cheung@accenture.com>
2018-08-13 17:40:11 +0800

    updated server.js

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const path = require("path");
 const app = express();
 
 app.use(express.static(__dirname + '/dist'))
-.get('/', (req, res) => res.sendFile('/index.html'))
+.get('/', (req, res) => res.sendFile(path.join(__dirname + '/dist' +'/index.html')))
 
 app.listen(process.env.PORT || 8080);
