e4739aa1c601c269a6f9bb5fc7ebb4ff18d0b994
Lucas Cheung <lucas.k.cheung@accenture.com>
2018-08-15 11:52:15 +0800

    finalized for heroku v1

--- server.js
+++ server.js
@@ -3,11 +3,6 @@ const path = require("path");
 const app = express();
 
 app.use(express.static(__dirname + '/dist'))
-.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-})
 .get('*', (req, res) => res.sendFile(path.join(__dirname + '/dist/index.html')))
 
 app.listen(process.env.PORT || 8080);
