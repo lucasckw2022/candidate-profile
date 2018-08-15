2aabcfee8bf3907a8b3ed60b3c619736a8f3c74b
Lucas Cheung <lucas.k.cheung@accenture.com>
2018-08-15 10:55:12 +0800

    updated for heroku

--- server.js
+++ server.js
@@ -2,13 +2,7 @@ const express = require('express');
 const path = require("path");
 const app = express();
 
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-})
+app
 .use(express.static(__dirname + '/dist'))
 .get('*', (req, res) => res.sendFile(path.join(__dirname + '/dist/index.html')))
 
