c7441f1c20d408ac6ee57660122bf4e892fa46f0
Lucas Cheung <lucas.k.cheung@accenture.com>
2018-08-15 11:50:21 +0800

    updated for heroku

--- src/components/comon/helper.js
+++ src/components/comon/helper.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export const APIHelper = (method, url, data = {}) => {
     return axios({
         method,
-        url: 'https://frozen-plateau-74954/' + url
+        url: 'https://frozen-plateau-74954.herokuapp.com/' + url
     })
 }
